d9c0fe7abf9d094bfd47c78a966c42fa78920be8
xavtgai <32963566+xavtgai@users.noreply.github.com>
2022-01-21 16:02:04 +0300

    format choice, levels
    выбор формата
    начало работы с уровнями

--- index.js
+++ index.js
@@ -2,8 +2,22 @@ const testword = Array.from(document.querySelectorAll('.testword'));
 const button1 = document.getElementById('button_1');
 const verdict = document.querySelector('.verdict');
 const popup = document.querySelector('.popup');
+const wordsLeft = document.querySelector('.numWordsLeft');
+const nextLevelButton = document.querySelector('.nextLevel');
+const levelChoice = document.getElementById('levelChoice');
 
 let headword = document.querySelector('.headword');
+
+let question = 'word';
+let answers = 'translation';
+
+//формат нужен - при проверке ответа; при показе хедворда; при показе ответов. 
+function setFormat(headword, answerOptions) {
+    
+    question = headword;
+    answers = answerOptions;
+    showNewWord ();
+}
 
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
@@ -21,21 +35,39 @@ function shuffle(array) {
 const slova = Array.from(words);  
 //слайс нужен чтобы создать отдельную копию массива
 let numOfWordsInALevel = 3;
-const numOfLevels = Math.floor(slova.length / numOfWordsInALevel)
+const numOfLevels = Math.floor(slova.length / numOfWordsInALevel);
+
+levelChoice.setAttribute('max', numOfLevels)
+
+let currentLevelNum = 0;
+
+function chooseLevel(value) {
+    
+    if (value > numOfLevels) {alert('bad level')}
+    currentLevelNum = value;
+    if (value === 'all') numOfWordsInALevel = slova.length;
+    showNewWord ();
+}
+// chooseLevel(1);
 
 console.log(numOfLevels, 'numOfL')
+let startOfTheLevel = numOfWordsInALevel * currentLevelNum;
+console.log(startOfTheLevel, 'start');
+//если остается еще несколько слов после последнего уровня, но на целый уровень не хватит,
+// подгеребем их в последний уровень
+let endOfTheLevel= startOfTheLevel +  numOfWordsInALevel;
+if (slova.length - endOfTheLevel < numOfWordsInALevel) endOfTheLevel = slova.length; 
 
-let oneLevel = shuffle(slova.slice())
-    
-console.log('onelevel', oneLevel );
+let currentLevel = shuffle(slova.slice(startOfTheLevel, endOfTheLevel ))
+wordsLeft.innerHTML = currentLevel.length;
+
+console.log('currentLevel', currentLevel );
+console.log(endOfTheLevel, 'endOfTheLevel');
 
 // Делаем массив со всеми ответами и при каждом новом слове надо приписывать кнопкам 
 //набор из 1 правильного ответа и остальных (по числу кнопок) неправильных.
 
-
-function chooseLevel() {
-    prompt('are you ready?')
-}
+//номер текущего уровня
 
 function showVerdict(result) 
 {
@@ -82,41 +114,47 @@ let numWrongAnswers = 0;
 let numCorrectAnswers = 0;
 
 function checkAnswer (e) {
-    console.log('smth', e.target.innerText);
-    if (e.target.innerText === headword.innerText) {
-        console.log('Правильно: ', headword.innerText, 'Вот и Твой ответ: ', e.target.innerText);
+   
+    if (e.target.innerText === currentLevel[0][answers]) {
+        console.log('Правильно: ', currentLevel[0][answers], 'Вот и Твой ответ: ', e.target.innerText);
         showVerdict('correct');
-        setTimeout(showNewWord, 2000);
-        oneLevel.shift();
+        setTimeout(showNewWord, 500);
+        currentLevel.shift();
         } else { 
             showVerdict('wrong');
-            setTimeout(showNewWord, 1000);
-            oneLevel.push(headword.innerText);
+            setTimeout(showNewWord, 500);
+            currentLevel.push(currentLevel[0]);
             numWrongAnswers +=1;
-            console.log('Правильно: ', headword.innerText, 'Твой ответ: ', e.target.innerText);
+            console.log('Правильно: ', currentLevel[0][answers], 'Твой ответ: ', e.target.innerText);
+            
     }
+    wordsLeft.innerHTML = currentLevel.length;
+    console.log('current one level', currentLevel)
 }
 
 const allWords = shuffle(slova);
-//(или может шаффл Onelevel?)
-
+//(или может шаффл currentLevel?)
 
 function showNewWord () {
     if (verdict.classList.contains('verdict_true')) {verdict.classList.remove('verdict_true')};
     if (verdict.classList.contains('verdict_wrong')) {verdict.classList.remove('verdict_wrong')};
-    console.log(oneLevel, 'onelevel');
-    console.log(slova, 'slova');
     
-    if (oneLevel.length !== 0)
-        {headword.innerText = oneLevel[0];
+    if (currentLevel.length !== 0)
+        
+        {
+            shuffle(currentLevel)
+            headword.innerText = currentLevel[0][question];
         let wordsForAnswerOptions = Array();
+        
             //добавляем в массив ответов правильное слово
-        wordsForAnswerOptions.push(headword.innerText);
-        for (let i=0; i< testword.length; i++ ) 
-            { if (allWords[i] !== headword.innerText)  wordsForAnswerOptions.push(allWords[i]) }
+        wordsForAnswerOptions.push(currentLevel[0][answers]);
+        let testwords_to_buttons = 1;
+
+        for (let i=0; testwords_to_buttons <= testword.length; i++ ) 
+            { if (allWords[i][question] !== headword.innerText)  
+                wordsForAnswerOptions.push(allWords[i][answers]);
+                testwords_to_buttons+=1 }
             shuffle(wordsForAnswerOptions);
-            console.log(wordsForAnswerOptions, 'options');
-            console.log(allWords, 'allwords');
 
         testword.forEach((word) => { 
             word.addEventListener('click', checkAnswer);
@@ -132,10 +170,10 @@ function showNewWord () {
 showNewWord ();
 
 function score (numOfWordsInTheLevel) {
-    if (numWrongAnswers === 0) {headword.innerText =  'Офигеть! Ваще без ошибок' }
+    if (numWrongAnswers === 0) {
+        headword.innerText =  'Офигеть! Ваще без ошибок' }
     else {headword.innerText =  `Ну ты и лузер, ошибок у тебя -  ${numWrongAnswers}`}
+//если существуют еще уровни, то показываем кнопку перехода на следующий уровень
+    if (currentLevelNum < numOfLevels) {nextLevelButton.classList.remove('invisible')};
 }
 
-function setFormat(headword, answerOptions) {
-    if (headword == 'word' && answerOption == 'translation') {}
-}
